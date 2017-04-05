1defe2f692726b5eaee0806686dec72261d1c070
Tomasz Marcinkowski <lordtomar@gmail.com>
2017-04-05 22:54:57 +0200

    Refactoring Fix

---
+++ Symulator/Symulator/Request/GetRequest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Symulator
+{
+    public class GetRequest : BaseRequest, IRequest
+    {
+        public GetRequest(String url) : base(url)
+        {
+            _requestMethod = "GET";
+        }
+
+        protected override void MakeRequest()
+        {
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(FullUrl);
+            request.Method = _requestMethod;
+            String test = String.Empty;
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                Stream dataStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(dataStream);
+                test = reader.ReadToEnd();
+                reader.Close();
+                dataStream.Close();
+            }
+        }
+    }
+}
---
+++ Symulator/Symulator/Request/PostRequest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Symulator
+{
+    public class PostRequest : BaseRequest, IRequest
+    {
+        public PostRequest(String url) : base (url)
+        {
+            _requestMethod = "POST";
+        }
+
+        protected override void MakeRequest()
+        {
+            var request = (HttpWebRequest)WebRequest.Create(_mainUrl);
+            var postData = _parameters;
+            var data = Encoding.ASCII.GetBytes(postData);
+            request.ContentLength = data.Length;
+
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+            var response = (HttpWebResponse)request.GetResponse();
+
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+        }
+    }
+}
